8314b6f64950bf43c88f7ccec17ba092dec6284f
Robert Worgul <robert.worgul@scitotec.de>
2020-11-14 19:18:45 +0100

    Highlight negative values

--- src/components/Calling.vue
+++ src/components/Calling.vue
@@ -3,7 +3,7 @@
       <v-col cols="12">
         [
         <span class="mx-2" v-for="(d, index) in data" :key="index">
-          <span>{{d.duration}}</span>
+          <span v-bind:class="{ 'pause-value': d.type === 'pause' }">{{d.duration}}</span>
           <span v-if="index + 1 < data.length">, </span>
         </span>
         ]
@@ -18,3 +18,9 @@ export default {
   props: ['data'],
 };
 </script>
+
+<style scoped>
+.pause-value {
+  color: #b3b3b3;
+}
+</style>
